0d32b78e26ec34be5fbd1f65fa7804f3ea1b002e
Shadab Ali1 <Shadab.Ali1@q3infotech.net>
2025-03-20 09:00:54 +0530

    Updated configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/Renetter/",
   css: {
     devSourcemap: true,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
   },
 });
